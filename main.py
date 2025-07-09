dff905877fc8b5c2eafe5b49e91a49e60d17e67e
Cosmin <mariana@gmail.com>
2025-07-09 10:19:08 -0500

    cosmin logic

--- main.py
+++ main.py
@@ -53,6 +53,53 @@ plt.figure(figsize=(12, 6))
 for label, group in full_df.groupby("type"):
     plt.plot(group["date"], group["total_distance"], label=label, marker='o', linestyle='--' if label == "Forecast" else '-')
 
+
+
+
+def extractCarID ():
+    return [
+        {"id": 1},
+        {"id": 2},
+        {"id": 3},
+        {"id": 4},
+        {"id": 5},
+        {"id": 6},
+        {"id": 7},
+        {"id": 8},
+        {"id": 9},
+        {"id": 10},
+
+        ]
+    
+def classify_car(car_id):
+    if 1<= car_id <= 7:
+        print ('The car is light.')
+
+    elif 8<= car_id <=10:
+        print ('The car is heavy.')
+    else:
+        print ('Invalid car ID.')
+
+def pick_car (passenger_count, vehicles):
+    light_vehicle = [v for v in vehicles if classify_car (v["id"]) == "light"]
+    heavy_vehicle = [v for v in vehicles if classify_car (v["id"]) == "heavy"]
+    
+    if passenger_count > 3:
+        return "heavy"
+    
+    if passenger_count <= 3:
+        all_light_vehicles_need_maintenance = all (v["need maintenance"] for v in light_vehicle)
+        all_heavy_vehicles_need_maintenance = all (v["need maintenance"] for v in heavy_vehicle)
+
+    if all_light_vehicles_need_maintenance:
+        if all_heavy_vehicles_need_maintenance:
+            return "light"
+        else:
+            return "heavy"
+    else: 
+        return "light"
+    
+
 plt.title("Total Distance Driven Per Month with Forecast")
 plt.xlabel("Date")
 plt.ylabel("Distance Driven")
